753e3e4895e768301045f234e4f628ddebb0cd6d
manupanand <manupanandindianinvestor@gmail.com>
2025-06-05 22:48:32 +0530

    updated

--- run.sh
+++ run.sh
@@ -1,29 +1,56 @@
+
 #!/bin/bash
-set -ex
+set -ex 
 
-# Clone the repo
-mkdir -p /app
-git clone $APP_GIT_URL /app
 
-# Optional: print structure
-ls -R /app
 
-# Load MongoDB schema(s)
-if [ "$DB_TYPE" == "mongo" ]; then
-  for file in $SCHEMA_FILES; do
-    echo "Loading Mongo schema: $file"
-    if [[ "$DB_HOST" == mongodb://* ]]; then
-      mongosh "$DB_HOST" < "/app/$file"
-    else
-      mongosh --host "$DB_HOST" < "/app/$file"
-    fi
-  done
+# Clone repo if not already cloned
+if [ ! -d "$REPO_DIR" ]; then
+    echo "Cloning repository..."
+    git clone "$APP_GIT_URL "
 fi
 
-# Load MySQL schema(s)
+
+
+if [ "$DB_TYPE" == "mongo" ]; then
+
+  # fallback to localhost if not set
+#schema-load/catalogue/db/master.js
+for service_dir in "$REPO_DIR"/*; do
+    DB_NAME=$(basename "$service_dir")
+    DB_PATH="$service_dir/db"
+    ls -la
+
+    if [ -d "$DB_PATH" ]; then
+        for js_file in "$DB_PATH"/*.js; do
+            [ -e "$js_file" ] || continue  # skip if no .js files
+            echo "📦 Importing $(basename "$js_file") into DB: $DB_NAME at $DB_HOST"
+            mongosh "mongodb://$DB_HOST:27017/$DB_NAME" < "$js_file"
+        done
+    else
+        echo "⚠️  No db/ folder found in $service_dir"
+    fi
+done
+fi
+###--------------shipping--
 if [ "$DB_TYPE" == "mysql" ]; then
-  for file in $SCHEMA_FILES; do
-    echo "Loading MySQL schema: $file"
-    mysql -h "$DB_HOST" -u"$DB_USER" -p"$DB_PASS" < "/app/$file"
-  done
+
+  # fallback to localhost if not set
+#schema-load/catalogue/db/master.js
+for service_dir in "$REPO_DIR"/*; do
+    DB_NAME=$(basename "$service_dir")
+    DB_PATH="$service_dir/db"
+    ls -la
+
+    if [ -d "$DB_PATH" ]; then
+        for sql_file in "$DB_PATH"/*.sql; do
+            [ -e "$sql_file" ] || continue  # skip if no .sql files
+            echo "📦 Importing $(basename "$sql_file") into DB: $DB_NAME at $DB_HOST"
+            mysql -h "$DB_HOST" -u"$DB_USER" -p"$DB_PASS" < "$sql_file"
+
+        done
+    else
+        echo "⚠️  No db/ folder found in $service_dir"
+    fi
+done
 fi
